7b04772013f01c874df29f10df62e092f8a9e58d
guisopo <production@tectonica.co>
2022-05-27 19:34:49 +0200

    Move menu icon to its own component

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
+import { MenuIcon } from '../MenuIcon'
 import './index.scss'
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="menu-icon">
-        <div className="lines-wrapper">
-          <span className="menu-line"></span>
-          <span className="menu-line"></span>
-          <span className="menu-line"></span>
-        </div>
-      </div>
+      <MenuIcon />
     </footer>
   )
 }
---
+++ src/Components/MenuIcon/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import './index.scss'
+
+export const MenuIcon = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleClick = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <div onClick={handleClick} className={`menu-icon ${menuOpen ? 'menu-icon--open' : null}`}>
+      <div className="lines-wrapper">
+        <span className="menu-line"></span>
+        <span className="menu-line"></span>
+        <span className="menu-line"></span>
+      </div>
+    </div>
+  )
+}
