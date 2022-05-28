c38156afb3f53367fa2552f376ec52bd874f60c7
guisopo <production@tectonica.co>
2022-05-28 18:31:16 +0200

    Fix grid styles and add icon animation

--- src/Components/GridItem/index.jsx
+++ src/Components/GridItem/index.jsx
@@ -8,12 +8,15 @@ export const GridItem = ({ game }) => {
 
   return (
     <div className="grid-item">
-      <Link to={`/game/${game.GameName.replace(/\s+/g, '-').toLowerCase()}`}>
+      <Link className="grid-item__link" to={`/game/${game.GameName.replace(/\s+/g, '-').toLowerCase()}`}>
+        
         <div className="grid-item__image-wrapper">
           <img className="grid-item__image" src={gameCover} alt="game cover" />
           {/* <img className="grid-item__image" src={game.FeaturedImageUrl} alt="game cover" /> */}
         </div>
+        
         <h3 className="grid-item__title">{game.GameName}</h3>
+
       </Link>
     </div>
   )
